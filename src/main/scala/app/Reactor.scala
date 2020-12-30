d17687fbe2506bd2a052d27ad56fb3273f43c09d
ryota0624 <tennis.suta6@gmail.com>
2020-12-31 00:04:21 +0900

    update scala

--- src/main/scala/app/Reactor.scala
+++ src/main/scala/app/Reactor.scala
@@ -3,7 +3,8 @@ package app
 import better.files.File
 import com.typesafe.scalalogging.LazyLogging
 import git.ChangedPumlFiles
-import notification.{ConsolePublisher, Notification, NotificationPublisher}
+import github.{CommentPublisher, RepositoryIdentifier}
+import notification.{Notification, NotificationPublisher}
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 
 import scala.util.Try
@@ -26,16 +27,25 @@ class Reactor(publisher: NotificationPublisher) extends LazyLogging {
   }
 }
 
-object Reactor extends Reactor(ConsolePublisher) with App {
-//  import org.kohsuke.github.GitHubBuilder
+object Reactor extends App with LazyLogging {
+  import org.kohsuke.github.GitHubBuilder
+
+  val github = GitHubBuilder.fromEnvironment.build
+
+  val instance = new Reactor(
+    new CommentPublisher(
+      new RepositoryIdentifier.Name(sys.env("REPOSITORY_NAME")),
+      sys.env("PR_NUMBER").toInt,
+      github
+    )
+  )
   val gitDir = Try(args(0)).getOrElse(sys.env("GITHUB_WORKSPACE"))
   val from = Try(args(1)).getOrElse(sys.env("FROM"))
   val to = Try(args(2)).getOrElse(sys.env("TO"))
 
   logger.info(s"gitDir $gitDir from $from to $to")
 
-  //  val github = GitHubBuilder.fromEnvironment.build
-  perform(gitDir, from, to).recover {
+  instance.perform(gitDir, from, to).recover {
     case t: Throwable =>
       logger.error(s"$t")
       sys.error(t.getMessage)
